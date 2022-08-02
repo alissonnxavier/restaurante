fc04808406ffbc4ddb3b3b0b61c089ea9b8133d8
Xavier <alissonalves29@gmail.com>
2022-08-02 13:28:28 -0300

    criado integraçao com banco de dados

---
+++ routes/admin.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next)=>{
+
+    res.render('admin/index', {
+
+    });
+});
+
+router.get('/contacts', (req, res, next)=>{
+
+    res.render('admin/contacts', {
+
+    });
+});
+
+router.get('/emails', (req, res, next)=>{
+
+    res.render('admin/emails', {
+
+    });
+});
+
+router.get('/login', (req, res, next)=>{
+
+    res.render('admin/login', {
+
+    });
+});
+
+router.get('/menus', (req, res, next)=>{
+
+    res.render('admin/menus', {
+
+    });
+});
+
+router.get('/reservations', (req, res, next)=>{
+
+    res.render('admin/reservations', {
+            date: ''
+    });
+});
+
+router.get('/users', (req, res, next)=>{
+
+    res.render('admin/users', {
+
+    });
+});
+
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,54 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/revervations');
+var contacts = require('./../inc/contacts');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  menus.getMenus().then(results =>{
+  menus.getMenus().then(results => {
 
     res.render('index', {
       title: 'Restaurante Saboroso',
       menus: results
     });
   })
-  
+
 });
 
-router.get('/contacts', (req, res, next)=>{
+router.get('/contacts', (req, res, next) => {
 
-  res.render('contact', { title: 'Contato'});
+  contacts.render(req, res);
 });
 
-router.get('/menus', (req, res, next)=>{
+router.post('/contacts', (req, res, next) => {
 
-  menus.getMenus().then(results =>{
+  if (!req.body.name) {
+    contacts.render(req, res, 'Insira o nome');
+  } else if (!req.body.email) {
+    contacts.render(req, res, 'Insira o email');
+  } else if (!req.body.message) {
+    contacts.render(req, res, 'Insira a menssagem')
+  } else {
+
+    contacts.save(req.body).then(results => {
+
+      req.body = {};
+
+      contacts.render(req, res, null, 'Menssagem enviada com sucesso');
+    }).catch(err => {
+
+      contacts.render(req, res, err.message);
+    })
+
+
+  }
+})
+
+router.get('/menus', (req, res, next) => {
+
+  menus.getMenus().then(results => {
 
     res.render('menu', {
 
@@ -34,19 +60,42 @@ router.get('/menus', (req, res, next)=>{
   })
 });
 
-router.get('/reservations', (req, res, next)=>{
+router.get('/reservations', (req, res, next) => {
 
-  res.render('reservation', {title: 'Reservas'});
+  reservations.render(req, res);
 });
 
-router.post('/reservations', (req, res, next)=>{
+router.post('/reservations', (req, res, next) => {
 
-  res.send(req.body);
+  if (!req.body.name) {
+    reservations.render(req, res, 'Insira o Nome');
+  } else if (!req.body.email) {
+    reservations.render(req, res, 'Insira o email');
+  } else if (!req.body.people) {
+    reservations.render(req, res, 'Insira o numero de pessoas');
+  } else if (!req.body.date) {
+    reservations.render(req, res, 'Insira a data');
+  } else if (!req.body.time) {
+    reservations.render(req, res, 'Selecione a hora');
+  } else {
+
+    reservations.save(req.body).then(results => {
+
+      req.body = {};
+
+      reservations.render(req, res, null, "Reserva realizada com sucesso!")
+
+    }).catch(err => {
+
+      reservations.render(req, res, err.message);
+    })
+
+  }
 })
 
-router.get('/services', (req, res, next)=>{
+router.get('/services', (req, res, next) => {
 
-  res.render('services', {title: 'Serviços'})
+  res.render('services', { title: 'Serviços' })
 })
 
 module.exports = router;
